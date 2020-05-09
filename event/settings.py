ebdfb8f3dc2739f00c46e09bd24f251deebe9024
manav014 <dpsman13016@gmail.com>
2020-05-10 01:31:19 +0530

    initial commit with some changes

--- event/settings.py
+++ event/settings.py
@@ -132,7 +132,7 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'studywithmanav2learn@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
